e51f963431890a0582f048198010ac2694d07a6f
Steve Hu <stevehu@StevedeMacBook-Pro.local>
2020-04-29 20:53:22 +0800

    20200429

--- python_array.py
+++ python_array.py
@@ -85,3 +85,27 @@ for j in i:
     print(j)
 
 data = [[0] * 4 for j in range(5)]
+
+class ArrayStack:
+
+    def __init__(self):
+        self._data = []
+
+    def __len__(self):
+        return len(self._data)
+
+    def is_empty(self):
+        return len(self._data) == 0
+
+    def push(self, e):
+        self._data.append(e)
+
+    def top(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]
+
+    def pop(self):
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()
